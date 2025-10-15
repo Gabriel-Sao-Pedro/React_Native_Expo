dcf9675c09705d13f35200a409657e0fd7424ce1
Gabriel São Pedro <gabrielosaopedro@gmail.com>
2025-10-15 01:04:43 -0300

    Projeto inicial: React Native Expo

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import {Text, ScrollView} from 'react-native'
+
+
+export default function App() {
+  return(
+    <ScrollView style={styles.container}>
+      <Text>
+        Hello world
+        opa
+      </Text>
+    </ScrollView>
+  )
+}
+
+const styles = {
+  container :{
+    backgroundColor: '#fff',
+  
+  }
+}
